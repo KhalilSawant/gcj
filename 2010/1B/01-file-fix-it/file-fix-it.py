016014f27e0133bf4f95e7b9b2002e13a0a61caa
Khalil Sawant <khalil.sawant@gmail.com>
2014-08-03 20:52:27 +0530

    GCJ 2010 Initial Commit

---
+++ 2010/1B/01-file-fix-it/file-fix-it.py
@@ -0,0 +1,27 @@
+import sys;
+import string;
+
+f_ip = open(sys.argv[1],'r');
+T = int(f_ip.readline());
+
+for i in range(T):
+	N, M = map(int, f_ip.readline().split());
+	
+	present = set();
+	
+	for j in range(N):		
+		path = f_ip.readline().rstrip();
+		while len(path):
+			present.add(path);
+			path, sep, rest = path.rpartition('/');
+
+	needed = 0;
+	for j in range(M):
+		path = f_ip.readline().rstrip();
+		while len(path) and path not in present:
+			present.add(path);
+			needed += 1;
+			path, sep, rest = path.rpartition('/');
+
+	print "Case #" + str(i+1) + ":",
+	print needed;
---
+++ 2010/qual/02-fair-warning/fair-warning.py
@@ -0,0 +1,26 @@
+########################################
+#	Author:	Khalil Sawant
+########################################
+
+import sys;
+from collections import deque;
+from operator import __sub__;
+from fractions import gcd;
+
+f_ip = open(sys.argv[1], 'r');
+C = int(f_ip.readline());
+
+for i in range(C):
+	t = deque( map( int, f_ip.readline().split() ) );
+	N = t.popleft(); # deque contains no of items as object[0]
+
+	pivot = t[0];
+	difference = [abs(x-pivot) for x in t];
+
+	T = reduce(gcd, difference);
+
+	if pivot % T == 0:
+		result = 0;
+	else:
+		result = T - (pivot % T);
+	print "Case #" + str(i+1) + ": " + str(result);
