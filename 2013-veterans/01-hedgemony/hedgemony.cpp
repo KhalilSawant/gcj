69be5056cecde61fcc83c79eb9657517d3399052
Khalil Sawant <khalil.sawant@gmail.com>
2014-08-05 00:53:12 +0530

    GCJ 2013 and 2014 Commits

---
+++ 2013-veterans/01-hedgemony/hedgemony.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+void show(double hedge[], int N);
+
+int main() {
+	int T;
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		int N;
+		cin >> N;
+		double hedge[N];
+		for (int j = 0; j < N; j++) {
+			cin >> hedge[j];
+		}
+		//        show(hedge, N);
+		for (int j = 1; j < N-1; j++) {
+			double calculated = (hedge[j-1]+hedge[j+1])/2;
+			if (hedge[j] > calculated) {
+				hedge[j] = calculated;
+			}
+			//            show(hedge, N);           
+		}
+		cout << "Case #" << (i+1) << ": " << hedge[N-2] << endl;
+	}
+}
+
+void show(double hedge[], int N) {
+	cout << "[";
+	for (int j = 0; j < N; j++) {
+		cout << hedge[j] << " ";
+	}
+	cout << "]" <<endl;
+}
